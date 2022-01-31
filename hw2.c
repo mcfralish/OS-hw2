c988c9dc8eeaddac5677cd75538bf30e00b786b8
mcfralish <mcfralish@gmail.com>
2022-01-30 23:41:00 -0500

    Commit of (should be) working files

--- hw2.c
+++ hw2.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-const char* file_name = "./F.txt";
+#include <stdlib.h>
+#include <stdbool.h>
 
-void process()
+const char* file_name = "./F.txt";
+int nreaders = 0;
+bool lock = false;
+
+void process(int counter)
 {
     pid_t pid; //process ID
+    while (lock){
+        sleep(1);
+        printf("Locked: %d cannot enter ", pid);
+    }
+    lock = true;
     FILE* file = fopen(file_name, "r"); //Open file
     int n =  getw(file);//Read number
-    printf("%d\n", n);
+    //printf("%d\n", n);
     fclose(file);
+
     if ((pid = getpid()) < 0){
         printf("unable to get pid");
     } else{
@@ -19,29 +30,24 @@ void process()
     n++;
     putw(n, file);
     fclose(file);
+    lock = false;
+    counter++;
+    if (counter<200){
+        process(counter);
+    }
 }
 
 int main()
 {  
-    if (fork() == 0){
-        printf("Starting Process P1\n");
-        int i = 0;
-        for (int i; i < 5; i++){
-            process();
+    
+
+    for (int i = 0; i < 3; i++){
+        if (fork() == 0){
+        printf("Starting Process %d\n", (i+1));
+        process(0);
+        exit(0);
         }
     }
-    if (fork() == 0){
-        printf("Starting Process P2\n");
-        int i = 0;
-        for (int i; i < 5; i++){
-            process();
-        }    }
-    if (fork() == 0){
-        printf("Starting Process P3\n");
-        int i = 0;
-        for (int i; i < 5; i++){
-            process();
-        }
-    }
+    
     return 0;
 }
