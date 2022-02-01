f2cc9ac2e9613a6a1b9ce4e0615a0d2e3d93044b
root <root@FrayLaptop.localdomain>
2022-01-31 19:21:43 -0500

    End of Schoolday, going to work from home

--- hw2.c
+++ hw2.c
@@ -3,23 +3,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <semaphore.h>
 
 const char* file_name = "./F.txt";
-int nreaders = 0;
-bool lock = false;
 
 void process(int counter)
 {
     pid_t pid; //process ID
-    while (lock){
-        sleep(1);
-        printf("Locked: %d cannot enter ", pid);
-    }
-    lock = true;
+    
     FILE* file = fopen(file_name, "r"); //Open file
-    int n =  getw(file);//Read number
+    char st_n[5];
+    fscanf(file, "%s", st_n);
     //printf("%d\n", n);
     fclose(file);
+    int n =  atoi(st_n);//Read number
 
     if ((pid = getpid()) < 0){
         printf("unable to get pid");
@@ -28,9 +25,11 @@ void process(int counter)
     }
     file = fopen(file_name, "w+");
     n++;
-    putw(n, file);
+    sprintf(st_n, "%d", n);
+    //itoa(n, st_n, 10);
+    //putw(n, file);
+    fprintf(file, "%s", st_n);
     fclose(file);
-    lock = false;
     counter++;
     if (counter<200){
         process(counter);
@@ -39,15 +38,12 @@ void process(int counter)
 
 int main()
 {  
-    
-
-    for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++){
         if (fork() == 0){
         printf("Starting Process %d\n", (i+1));
         process(0);
         exit(0);
         }
     }
-    
-    return 0;
+        return 0;
 }
